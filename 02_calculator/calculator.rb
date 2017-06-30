821b512cfce5f839a96ee1270d22e22b87d9a105
John Smith <alistoffish@gmail.com>
2017-06-30 13:16:36 +0100

    First two stages, all tests passed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
-#write your code here
+def add(x,y)
+  return x + y
+end
+
+def subtract(x,y)
+  return x - y
+end
+
+def sum(array)
+  return array.inject(0, :+)
+end
+
+def multiply(*args)
+  total = 1
+  args.each {|x| total *= x}
+  return total
+end
+
+def power(x,y)
+  return x**y
+end
+
+def factorial(n)
+  total = 1
+  array = []
+
+  if n == 0
+    return 0
+  else
+    while n > 0
+      array.push(n)
+      n -= 1
+    end
+  end
+
+  array.each{|x| total *= x}
+  return total
+end
