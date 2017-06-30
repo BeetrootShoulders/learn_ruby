917361708af824aff32190a9ade51939cf582230
John Smith <alistoffish@gmail.com>
2017-06-30 14:19:02 +0100

    simon says complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(word)
+  return word
+end
+
+def shout(word)
+  return word.upcase
+end
+
+def repeat(word, num = 2)
+  return ([word] * num).join(' ')
+end
+
+def start_of_word(word,num)
+  word[0...num]
+end
+
+def first_word(phrase)
+  phrase.partition(" ").first
+end
+
+LITTLE_WORDS = ["the", "over", "an", "and"]
+def titleize words
+	phrase = words.split(' ')
+	phrase.each do |word|
+		if LITTLE_WORDS.index(word) != nil && phrase.index(word) > 0
+			word
+		else
+			word.capitalize!
+		end
+	end
+	return phrase.join(' ')
+end
